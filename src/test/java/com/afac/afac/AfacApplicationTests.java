ff4a84edf5798f1219e34a0f0c2284d2dfc9fc76
Kara <blk20100@gmail.com>
2023-05-24 12:32:52 +0200

    first commit

---
+++ src/test/java/com/afac/afac/AfacApplicationTests.java
@@ -0,0 +1,13 @@
+package com.afac.afac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AfacApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
